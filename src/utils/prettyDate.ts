52a38c043df316b0c39e7558ad42afbc850c0806
Jacob <millerjr@tcd.ie>
2025-07-16 12:09:34 -0300

    removing console logs

--- src/utils/prettyDate.ts
+++ src/utils/prettyDate.ts
@@ -4,7 +4,6 @@ import getLocalDateFromYYYYMMDD from "./getLocalDateFromYYYMMDD";
 export default function prettyDate(
   stringDate: string
 ): string {
-  console.log(stringDate, formatDate(new Date()));
   if (stringDate === formatDate(new Date())) return "Today";
 
   return getLocalDateFromYYYYMMDD(
