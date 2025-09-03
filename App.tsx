46fd9cbf534ecb839ef30c07c153c54789cd8e31
Jacob <millerjr@tcd.ie>
2025-09-03 11:03:31 -0300

    force app reload on focus

--- App.tsx
+++ App.tsx
@@ -4,13 +4,34 @@ import store, { persistor } from "./src/store";
 import RootNavigator from "./src/navigation/RootNavigator";
 import { PersistGate } from "redux-persist/integration/react";
 import Toast from "react-native-toast-message";
-import { Platform, View } from "react-native";
+import { AppState, Platform, View } from "react-native";
 import BGContainer from "./src/componentes/BGContainer";
 import AdaptiveAppContainer from "./src/componentes/AdaptiveAppContainer";
+import { useEffect, useRef, useState } from "react";
 
 export default function App() {
+  // Force app to reload when focused
+  const [appKey, setAppKey] = useState(0);
+  const appState = useRef(AppState.currentState);
+  useEffect(() => {
+    const sub = AppState.addEventListener(
+      "change",
+      (next) => {
+        if (
+          appState.current.match(/inactive|background/) &&
+          next === "active"
+        ) {
+          // Increment key to force remount
+          setAppKey((k) => k + 1);
+        }
+        appState.current = next;
+      }
+    );
+    return () => sub.remove();
+  }, []);
+
   return (
-    <AdaptiveAppContainer>
+    <AdaptiveAppContainer key={appKey}>
       <BGContainer>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
