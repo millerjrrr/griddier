766edbbeb6355f344640208e730574203fc8a760
Jacob <millerjr@tcd.ie>
2026-01-16 11:55:39 -0300

    added masked view for clean scrolling

--- src/screens/Ranges/MyRanges.tsx
+++ src/screens/Ranges/MyRanges.tsx
@@ -1,5 +1,6 @@
 import { WhiteTextBold } from "@src/componentes/AppText";
-import FadeBackgroundView from "@src/componentes/FadeBackgroundView";
+import MaskedView from "@react-native-masked-view/masked-view";
+import { LinearGradient } from "expo-linear-gradient";
 import DeleteModal from "@src/componentes/Modals/DeleteModal";
 import RangeModal from "@src/componentes/Modals/RangeModal";
 import RangeCard from "@src/componentes/RangeCard";
@@ -96,39 +97,56 @@ const MyRanges = () => {
           dataEntry={selectedEntry}
           onClose={closeDeleteModal}
         />
+
         {data.length > 0 ? (
-          <FlatList
-            data={data}
-            extraData={dataEntries}
-            renderItem={({ item }) => {
-              // must be called item for FlatList to work
-              return (
-                <RangeCard
-                  dataEntry={item}
-                  selectFunction={() => openModal(item)}
-                  showDeleteModal={() =>
-                    openDeleteModal(item)
-                  }
-                  showStackSize={showStackSize}
-                />
-              );
-            }}
-            keyExtractor={(item) => item.gridName}
-            style={{
-              flex: 1,
-              width: "100%",
-              paddingVertical: 20 * base,
-              paddingHorizontal: 15 * base,
-              backgroundColor: "transparent",
-            }}
-            showsVerticalScrollIndicator={false}
-            contentContainerStyle={{
-              paddingTop: filter.activated
-                ? 160 * base
-                : 50 * base,
-              paddingBottom: 80 * base,
-            }}
-          />
+          <MaskedView
+            style={{ flex: 1 }}
+            maskElement={
+              <LinearGradient
+                colors={[
+                  "transparent",
+                  "black",
+                  "black",
+                  "transparent",
+                ]}
+                locations={[0, 0.15, 0.95, 1]}
+                style={{ flex: 1 }}
+              />
+            }
+          >
+            <FlatList
+              data={data}
+              extraData={dataEntries}
+              renderItem={({ item }) => {
+                // must be called item for FlatList to work
+                return (
+                  <RangeCard
+                    dataEntry={item}
+                    selectFunction={() => openModal(item)}
+                    showDeleteModal={() =>
+                      openDeleteModal(item)
+                    }
+                    showStackSize={showStackSize}
+                  />
+                );
+              }}
+              keyExtractor={(item) => item.gridName}
+              style={{
+                flex: 1,
+                width: "100%",
+                paddingVertical: 20 * base,
+                paddingHorizontal: 15 * base,
+                backgroundColor: "transparent",
+              }}
+              showsVerticalScrollIndicator={false}
+              contentContainerStyle={{
+                paddingTop: filter.activated
+                  ? 160 * base
+                  : 50 * base,
+                paddingBottom: 80 * base,
+              }}
+            />
+          </MaskedView>
         ) : (
           <View style={{ flex: 1, paddingTop: 50 * base }}>
             <WhiteTextBold s={24 * base}>
--- src/screens/Ranges/components/ControlButton.tsx
+++ src/screens/Ranges/components/ControlButton.tsx
@@ -12,7 +12,7 @@ const ControlButton: React.FC<{
   return (
     <AppTouchable
       style={{
-        backgroundColor: `${colors.BG1}c0`,
+        backgroundColor: colors.BG1,
         borderRadius: 5 * base,
         padding: 2 * base,
         marginHorizontal: 5 * base,
